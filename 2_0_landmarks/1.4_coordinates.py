c11d1218c1b4713d3f465ef9f02ab775fc76ad9e
iws-seed <iwstechnical7@gmail.com>
2026-04-19 14:12:28 +0100

    added landmarks sketiching coordinate options

--- 2_0_landmarks/1.4_coordinates.py
+++ 2_0_landmarks/1.4_coordinates.py
@@ -1,47 +1,23 @@
-# step2_coordinates.py
-
 import cv2
 import mediapipe as mp
 import time
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-base_options = python.BaseOptions(
+
+
+base_options=python.BaseOptions(
     model_asset_path="models/pose_landmarker_lite.task"
 )
 
-options = vision.PoseLandmarkerOptions(
+
+print(f"Observing the base options: {base_options}")
+
+# RESULT: Observing the base options: BaseOptions(model_asset_path='models/pose_landmarker_lite.task', model_asset_buffer=None, delegate=None)
+
+options=vision.PoseLandmarkerOptions(
     base_options=base_options,
     running_mode=vision.RunningMode.VIDEO
 )
 
-landmarker = vision.PoseLandmarker.create_from_options(options)
-
-cap = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
-
-    timestamp = int(time.time() * 1000)
-    result = landmarker.detect_for_video(mp_image, timestamp)
-
-    if result.pose_landmarks:
-        lm = result.pose_landmarks[0]
-
-        # Print nose and left wrist
-        print("Nose:", lm[0].x, lm[0].y)
-        print("Left Wrist:", lm[15].x, lm[15].y)
-
-    cv2.imshow("Step 2: Coordinates", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+print(f"Observing the pose landmarker options: {options}")
